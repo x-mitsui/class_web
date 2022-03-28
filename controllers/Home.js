39f1799e5d15421f0b91a9c8896dfa03a25b6e83
dzq <x_mitsui@163.com>
2022-03-28 22:18:47 +0800

    controller中间件

--- controllers/Home.js
+++ controllers/Home.js
@@ -1,9 +1,22 @@
 const page_config = require('../configs/page')
+const navData = require('../configs/nav')
+const { IMG_BASE_URL } = require('../configs/url')
 
+const { getSliderData } = require('../services/Slider')
 class Index {
   async index(ctx, next) {
+    // 通过ejs渲染出了HTML，
+    // 但是此HTML需要的css，js，img静态资源需要webpack打包提供，
+    // 当然webpack不需要产生HTML
+    // 总之：前端请求此服务器路由http:/localhost:3003/
+    // 此无服务器渲染出HTML给前端
+
+    const sliderData = await getSliderData()
     await ctx.render('index', {
-      CONFIG: page_config.INDEX
+      CONFIG: page_config.INDEX,
+      navData,
+      sliderData,
+      IMG_BASE_URL
     })
   }
 
