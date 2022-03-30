d760800bb8af02ec190f83d73e1c2910d8f37049
dzq <x_mitsui@163.com>
2022-03-30 23:43:32 +0800

    controller增加相关

--- controllers/Home.js
+++ controllers/Home.js
@@ -3,21 +3,30 @@ const navData = require('../configs/nav')
 const { IMG_BASE_URL } = require('../configs/url')
 
 const { getSliderData } = require('../services/Slider')
+const { getRecomCourseData } = require('../services/RecomCourse')
+const { getCollectionData } = require('../services/Collection')
 class Index {
   async index(ctx, next) {
     // 通过ejs渲染出了HTML，
     // 但是此HTML需要的css，js，img静态资源需要webpack打包提供，
     // 当然webpack不需要产生HTML
-    // 总之：前端请求此服务器路由http:/localhost:3003/
+    // 总之：前端请求此服务器路由http:/localhost:3004/
     // 此无服务器渲染出HTML给前端
 
     const sliderData = await getSliderData()
-    await ctx.render('index', {
-      CONFIG: page_config.INDEX,
-      navData,
-      sliderData,
-      IMG_BASE_URL
-    })
+    const recomCourseData = await getRecomCourseData()
+    const collectionData = await getCollectionData()
+    // console.log('collectionData:', collectionData)
+    // note 调试小技巧，用ctx body调试
+    ctx.body = collectionData
+    // await ctx.render('index', {
+    //   CONFIG: page_config.INDEX,
+    //   navData,
+    //   sliderData,
+    //   recomCourseData,
+    //   collectionData,
+    //   IMG_BASE_URL
+    // })
   }
 
   async list(ctx, next) {
