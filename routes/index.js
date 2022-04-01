b73b5a6aace8de62cb687557134bdad872e74b73
dzq <x_mitsui@163.com>
2022-04-01 13:38:20 +0800

    路由参数语义化

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const IndexController = require('../controllers/Index')
 const ListController = require('../controllers/List')
 const ErrorController = require('../controllers/Error')
 router.get('/', IndexController.render)
-router.get('/list/:id?', ListController.render)
+router.get('/list/:kw?', ListController.render)
 router.get('*', ErrorController.render)
 
 module.exports = router
